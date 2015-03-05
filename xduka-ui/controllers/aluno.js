311cccf65a17df3865c9ad0c64c74cbc19ec5382
douglasbolis <douglasbolislima@gmail.com>
2015-03-05 19:47:01 -0300

    Adicionado rotas e controllers restantes de Alunos

--- xduka-ui/controllers/aluno.js
+++ xduka-ui/controllers/aluno.js
@@ -1,10 +1,43 @@
 
 module.exports = function () {
-    var messages = require('../mockup/xduka-json/aluno/messages.json'),
+    var aniversariantes = require('../mockup/xduka-json/aluno/aniversariantes.json'),
+        conteudo = require('../mockup/xduka-json/aluno/conteudo.json'),
+        editarPerfil = require('../mockup/xduka-json/aluno/editar-perfil.json'),
+        grade = require('../mockup/xduka-json/aluno/grade.json'),
+        horarios = require('../mockup/xduka-json/aluno/horarios.json'),
+        mensagens = require('../mockup/xduka-json/aluno/messages.json'),
+        notas = require('../mockup/xduka-json/aluno/notas.json'),
+        parcelas = require('../mockup/xduka-json/aluno/parcelas.json'),
+        tarefas = require('../mockup/xduka-json/aluno/tasks.json'),
+
         controller = {};
 
+    controller.showAniversariantes = function (req, res) {
+        res.json(aniversariantes);
+    };
+    controller.showConteudo = function (req, res) {
+        res.json(conteudo);
+    };
+    controller.showEditarPerfil = function (req, res) {
+        res.json(editarPerfil);
+    };
+    controller.showGrade = function (req, res) {
+        res.json(grade);
+    };
+    controller.showHorarios = function (req, res) {
+        res.json(horarios);
+    };
     controller.showMessages = function (req, res) {
-        res.json(messages);
+        res.json(mensagens);
+    };
+    controller.showNotas = function (req, res) {
+        res.json(notas);
+    };
+    controller.showParcelas = function (req, res) {
+        res.json(parcelas);
+    };
+    controller.showTarefas = function (req, res) {
+        res.json(tarefas);
     };
 
     return controller;
--- xduka-ui/routes/aluno.js
+++ xduka-ui/routes/aluno.js
@@ -1,5 +1,39 @@
 module.exports = function (app) {
-    var controller = require('../controllers/aluno');
+    var controller = app.controllers.aluno;
+
+    //aniversariantes
+    app.route('/aniversariantes')
+        .get(controller.showAniversariantes);
+
+    //conteudo
+    app.route('/conteudo')
+        .get(controller.showConteudo);
+
+    //editar-perfil
+    app.route('/editar-perfil')
+        .get(controller.showEditarPerfil);
+
+    //grade
+    app.route('/grade')
+        .get(controller.showGrade);
+
+    //Horarios
+    app.route('/horarios')
+        .get(controller.showHorarios);
+
+    //mensagens
     app.route('/mensagens')
         .get(controller.showMessages);
+
+    //Notas
+    app.route('/notas')
+        .get(controller.showNotas);
+
+    //Parcelas
+    app.route('/parcelas')
+        .get(controller.showParcelas);
+
+    //Tarefas
+    app.route('/tarefas')
+        .get(controller.showTarefas);
 };
