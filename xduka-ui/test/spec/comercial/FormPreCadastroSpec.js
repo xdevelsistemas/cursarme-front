cabcf1750251e01d5be1d17397c7003b113aaa48
douglasbolis <douglas.lima@xdevel.com.br>
2015-06-03 19:17:05 -0300

    Ajustado arquivo de testes. Testes iniciais funcionando.

--- xduka-ui/test/spec/comercial/FormPreCadastroSpec.js
+++ xduka-ui/test/spec/comercial/FormPreCadastroSpec.js
@@ -1,29 +1,33 @@
-define(['app', 'angular', 'angularMocks', 'angularResource'], function(app, angular) {
-    console.log("\n\n'Running test to controller FormPreCadastro");
+describe('FormPreCadastro', function() {
+    // load the relevant application modules then load a special
+    // test module which overrides the $window with a mock version,
+    // so that calling window.alert() will not block the test
+    // runner with a real alert box. This is an example of overriding
+    // configuration information in tests.
 
-    describe('Controller: FormPreCadastro', function() {
-        // load the relevant application modules then load a special
-        // test module which overrides the $window with a mock version,
-        // so that calling window.alert() will not block the test
-        // runner with a real alert box. This is an example of overriding
-        // configuration information in tests.
+    var $scope;
 
-        beforeEach(module(app));
-
-        var $scope, ctrl;
-
-        beforeEach(inject(function($routeScope, $controller){
-            $scope = $routeScope.$new();
-            console.log('scope1', $scope);
-            ctrl = $controller('FormPreCadastro', {$scope: $scope})
-        }));
-
-        it('sets variables ', function() {
-            expect($scope).toBeDefined();
-            /*expect($scope.cor).toEqual('blue');
-             expect($scope.casa).toEqual('ap');*/
+    beforeEach(function() {
+        module('app');
+        inject(function($injector) {
+            $scope = $injector.get('$rootScope').$new();
         });
     });
+
+    it('cor definida', inject(function($controller) {
+        $controller('FormPreCadastro', {"$scope": $scope});
+        expect($scope.cor).toBeDefined();
+    }));
+
+    it('minha casa é azul', inject(function($controller) {
+        $controller('FormPreCadastro', {"$scope": $scope});
+        expect($scope.cor).toEqual('blue');
+    }));
+
+    it('minha casa é um ape', inject(function($controller) {
+        $controller('FormPreCadastro', {"$scope": $scope});
+        expect($scope.casa).toEqual('ape');
+    }));
 });
 
 /*
