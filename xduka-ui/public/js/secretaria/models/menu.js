ae31f10faca17be666dd672701c3879c43e023f8
davidpvilaca <davidpvilaca@hotmail.com>
2015-06-09 16:21:47 -0300

    Criado pagina secretaria

---
+++ xduka-ui/public/js/secretaria/models/menu.js
@@ -0,0 +1,19 @@
+(function () {
+    //Menu
+    'use strict';
+
+    angular.module('app.values').value('modelMenu', {
+        "list": [
+            {
+                "text": "Secretaria",
+                "iconClass": "entypo-docs",
+                "href": "#/secretaria"
+            },
+            {
+                "text": "Confirmação de Matrícula",
+                "iconClass": "entypo-docs",
+                "href": "#/secretaria/confirmarMatricula"
+            }
+        ]
+    })
+})();
