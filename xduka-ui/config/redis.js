dcbef16b514e5315fe102fdfc248d0bb0a30c80c
clayton@xdevel.com.br <clayton@xdevel.com.br>
2015-06-20 04:17:01 -0300

    - retirado a configuracao do app.js e passado para o redis, o modulo ira retornar o redis conectado para iniciar controle sessao

--- xduka-ui/config/redis.js
+++ xduka-ui/config/redis.js
@@ -1,6 +1,15 @@
+var redisClient = require('redis');
+
+// get enviroment variables
+var redis_host = process.env.REDIS_HOST;
+var redis_port = process.env.REDIS_PORT;
+var redis_db = process.env.REDIS_DB;
+
+
 module.exports = {
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-    db: process.env.REDIS_DB,
-    ttl: (60000 * 24 * 30)
+    host: redis_host,
+    port: redis_port,
+    db: redis_db,
+    ttl: (60000 * 24 * 30),
+    redisClient: redisClient.createClient(redis_port,redis_host) // connect to redis
 };
