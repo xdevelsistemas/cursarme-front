e1ef27a5fbbe4d624416c508a152265c7cf47d51
clayton@xdevel.com.br <clayton@xdevel.com.br>
2015-06-20 00:32:04 -0300

    criado ambiente de desenvolvimento/producao em um unico ambiente utilizando variaveis de ambiente

--- xduka-ui/config/dev/redis.js
+++
@@ -1,9 +0,0 @@
-var redisClient = require('redis').createClient();
-
-module.exports = {
-    host: 'localhost',
-    port: 6379,
-    db: 0,
-    ttl: (60000 * 24 * 30),
-    client: redisClient
-};
