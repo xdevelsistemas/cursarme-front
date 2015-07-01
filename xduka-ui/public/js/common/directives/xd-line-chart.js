dfb9286245a499c56189233706c39eed395a04e3
davidpvilaca <davidpvilaca@hotmail.com>
2015-07-01 19:41:45 -0300

    iniciado mudança da directiva xd-line-chart para c3

--- xduka-ui/public/js/common/directives/xd-line-chart.js
+++ xduka-ui/public/js/common/directives/xd-line-chart.js
@@ -16,14 +16,14 @@
                         $timeout(function(){
                             // Line Charts
                             var line_chart_xd = $('#'+scope.params.id);
-                            var line_chart = Morris.Line({
+                            /*var line_chart = Morris.Line({
                                 element: scope.params.id,   //str
                                 data: scope.params.data,    //list of dics
                                 xkey: scope.params.xkey,    //str
                                 ykeys: scope.params.ykeys,   //list
                                 labels: scope.params.labels, //list
                                 redraw: scope.params.redraw //boolean
-                            });
+                            });*/
 
                             line_chart_xd.parent().attr('style', '');
                             $('#load_'+scope.params.id).attr({style: 'display: none'});
