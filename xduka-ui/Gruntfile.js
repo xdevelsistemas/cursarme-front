3cca6e3754f0c1bf49fc3cd1a6735c9a6b65202d
douglasbolis <douglas.lima@xdevel.com.br>
2015-07-17 13:55:40 -0300

    Retirado requisição de módulo usemin e ng-annotate do Gruntfile.

--- xduka-ui/Gruntfile.js
+++ xduka-ui/Gruntfile.js
@@ -237,6 +237,4 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-usemin');
-    grunt.loadNpmTasks('grunt-ng-annotate');
 };
