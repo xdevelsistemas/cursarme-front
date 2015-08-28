cf0caa4c5f93c7ffa86b9b155a6102e8ecee2260
douglasbolis <douglas.lima@xdevel.com.br>
2015-08-28 17:24:20 -0300

    Ajustado renderização de erro 400 para desenv e produc;

--- xduka-ui/app.js
+++ xduka-ui/app.js
@@ -107,13 +107,25 @@ if (app.get('env') === 'development') {
 
 // development error handler
 // will print stacktrace
-app.use(function (err, req, res, next) {
-    res.status(err.status || 400);
-    res.render('error', {
-        message: err.message,
-        error: err
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 400);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-});
+}else {
+    // production error handler
+    // no stacktraces leaked to user
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 400);
+        res.render('400', {
+            message: err.message,
+            error: err
+        });
+    });
+}
 
 // development error handler
 // will print stacktrace
